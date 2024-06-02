83e2e881adb720407a557628ac8d7f920c2f1ec7
quidy0 <160316930+quidy0@users.noreply.github.com>
2024-06-02 14:28:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,27 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
+
+BetterBST<std::string> constructTree(const char* filename) {
   // поместите сюда свой код
+ BetterBST<std::string> tree;
+ std::ifstream file(filename);
+    if (!file.is_open()) {
+ std::cerr << "Error opening file." << std::endl;
+        return tree;
+    }
+
+ std::string word;
+    while (file >> word) {
+        for (char& c : word) {
+ c = std::tolower(c);
+            if (!isalpha(c)) {
+ c = ' ';
+            }
+        }
+ tree.insert(word);
+    }
+
+ file.close();
+    return tree;
 }
